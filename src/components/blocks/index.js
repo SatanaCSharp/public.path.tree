18a73fc0032c052e3d003e3378986f4cc5347e21
Yurii Horchuk <fkca.av15gyua@gmail.com>
2019-12-02 14:05:54 +0200

    added loader

--- src/components/blocks/index.js
+++ src/components/blocks/index.js
@@ -1,9 +1,11 @@
 import HeaderBlock from './Header';
 import FooterBlock from './Footer';
+import Loader from './Loader';
 import * as Buttons from './Buttons';
 
 export {
     HeaderBlock,
     FooterBlock,
-    Buttons
+    Buttons,
+    Loader
 }
