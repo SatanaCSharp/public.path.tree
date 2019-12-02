c724d1f0529e1d01a541da4f9c7a5deba825bdbb
Yurii Horchuk <fkca.av15gyua@gmail.com>
2019-12-02 17:42:37 +0200

    Create form layout

--- src/components/blocks/Buttons/index.jsx
+++ src/components/blocks/Buttons/index.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { FiX } from "react-icons/fi";
-
-const SuccessButton = ({buttonTitle, handleButtonClick, }) => (
+import PropTypes  from 'prop-types';
+const SuccessButton = ({buttonTitle, handleButtonClick }) => (
     <button className={"success-btn "} onClick={  handleButtonClick}>{buttonTitle}</button>
 );
 const CloseButton = ({ handleButtonClick }) => (
     <FiX size={25} color={"#6c757d"} onClick={handleButtonClick}/>
 );
+
+
+SuccessButton.propTypes = {
+    buttonTitle: PropTypes.string.isRequired,
+    handleButtonClick: PropTypes.func.isRequired
+};
+
+CloseButton.propTypes = {
+    handleButtonClick: PropTypes.func.isRequired
+};
+
+
 
 export {
     SuccessButton,
--- src/components/blocks/index.js
+++ src/components/blocks/index.js
@@ -2,10 +2,11 @@ import HeaderBlock from './Header';
 import FooterBlock from './Footer';
 import Loader from './Loader';
 import * as Buttons from './Buttons';
-
+import * as Inputs from './Inputs';
 export {
     HeaderBlock,
     FooterBlock,
     Buttons,
-    Loader
+    Loader,
+    Inputs
 }
